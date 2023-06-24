720ba3922d0efbb5825777a859ba9236a04a9cbf
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2023-06-24 16:09:09 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
-from .views import home, salvar, editar, delete, pesquisar
+from .views import home, pesquisar, salvar, editar, update, delete
 
 urlpatterns = [
     path('', home, name='home'),
-    path('salvar/', salvar, name='salvar'),
+    path('salvar/', salvar, name="salvar"),
     path('editar/<int:id>', editar, name='editar'),
+    path('update/<int:id>', update, name='update'),
     path('delete/<int:id>', delete, name='delete'),
     path('pesquisar/', pesquisar, name='pesquisar'),
 ]
