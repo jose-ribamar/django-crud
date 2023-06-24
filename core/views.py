4810b3ceabd50346b9c020f3ab16b7d866f71667
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2023-06-24 00:24:50 -0300

    Update views.py
    parte em que salva os dados e direciona ao banco de dados

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Pessoa
 
-# Create your views here.
 def home(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {"pessoas": pessoas})
+
+def salvar(request):
+    nome = request.POST.get("nome")
+    sobrenome = request.POST.get("sobrenome")
+    email = request.POST.get("email")
+    data_nascimento = request.POST.get("data_nascimento")
+    
+    pessoa = Pessoa.objects.create(nome=nome, sobrenome=sobrenome, email=email, data_nascimento=data_nascimento)
+    
     pessoas = Pessoa.objects.all()
     return render(request, "index.html", {"pessoas": pessoas})
