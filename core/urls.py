6716f5f6739aa8e339c84c917b50d7dfc5695b78
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2023-06-24 14:25:31 -0300

    Update urls.py
    adição de editar, pesquisar e deletar

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
 from django.urls import include, path
-from .views import home, salvar
+from .views import home, salvar, editar, delete, pesquisar
 
 urlpatterns = [
-    path('', home),
-    path('salvar/', salvar, name="salvar"),
+    path('', home, name='home'),
+    path('salvar/', salvar, name='salvar'),
+    path('editar/<int:id>', editar, name='editar'),
+    path('delete/<int:id>', delete, name='delete'),
+    path('pesquisar/', pesquisar, name='pesquisar'),
 ]
