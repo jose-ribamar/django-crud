d87eb070b3d081e2474006860fa43690c4b2a678
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2023-06-24 16:09:27 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,33 +1,44 @@
 from django.shortcuts import render, redirect
 from .models import Pessoa
 
+from django.db.models import Q
+
 def home(request):
     pessoas = Pessoa.objects.all()
     return render(request, "index.html", {"pessoas": pessoas})
 
 def salvar(request):
-    if request.method == 'POST':
-        nome = request.POST.get("nome")
-        sobrenome = request.POST.get("sobrenome")
-        email = request.POST.get("email")
-        data_nascimento = request.POST.get("data_nascimento")
-        
-        pessoa = Pessoa.objects.create(nome=nome, sobrenome=sobrenome, email=email, data_nascimento=data_nascimento)
-        
-        return redirect('home')
+    nome = request.POST.get("nome")
+    sobrenome = request.POST.get("sobrenome")
+    email = request.POST.get("email")
+    data_nascimento = request.POST.get("data_nascimento")
     
-    return render(request, "index.html")
+    pessoa = Pessoa.objects.create(nome=nome, sobrenome=sobrenome, email=email, data_nascimento=data_nascimento)
+    
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {"pessoas": pessoas})
 
 def editar(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request, "update.html", {"pessoa": pessoa})
+
+
+def update(request, id):
     pessoa = Pessoa.objects.get(id=id)
     
     if request.method == 'POST':
         pessoa.nome = request.POST.get("nome")
         pessoa.sobrenome = request.POST.get("sobrenome")
         pessoa.email = request.POST.get("email")
-        pessoa.data_nascimento = request.POST.get("data_nascimento")
+        
+        data_nascimento = request.POST.get("data_nascimento")
+        if data_nascimento:
+            pessoa.data_nascimento = data_nascimento
+        
         pessoa.save()
         return redirect('home')
+    
+    return render(request, "update.html", {"pessoa": pessoa})
     
     return render(request, "update.html", {"pessoa": pessoa})
 
@@ -39,7 +50,12 @@ def delete(request, id):
 def pesquisar(request):
     if request.method == 'GET':
         termo_pesquisa = request.GET.get("search_query")
-        resultados = Pessoa.objects.filter(nome__icontains=termo_pesquisa)
+        resultados = Pessoa.objects.filter(
+            Q(nome__icontains=termo_pesquisa) |
+            Q(sobrenome__icontains=termo_pesquisa) |
+            Q(email__icontains=termo_pesquisa) |
+            Q(data_nascimento__icontains=termo_pesquisa)
+        )
         return render(request, "index.html", {"pessoas": resultados})
     
     return redirect('home')
