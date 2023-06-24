6c23a91647618c91d424d3d6c313c475273803f2
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2023-06-24 14:25:24 -0300

    Update views.py
    adição de editar, pesquisar e deletar

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pessoa
 
 def home(request):
@@ -6,12 +6,40 @@ def home(request):
     return render(request, "index.html", {"pessoas": pessoas})
 
 def salvar(request):
-    nome = request.POST.get("nome")
-    sobrenome = request.POST.get("sobrenome")
-    email = request.POST.get("email")
-    data_nascimento = request.POST.get("data_nascimento")
+    if request.method == 'POST':
+        nome = request.POST.get("nome")
+        sobrenome = request.POST.get("sobrenome")
+        email = request.POST.get("email")
+        data_nascimento = request.POST.get("data_nascimento")
+        
+        pessoa = Pessoa.objects.create(nome=nome, sobrenome=sobrenome, email=email, data_nascimento=data_nascimento)
+        
+        return redirect('home')
     
-    pessoa = Pessoa.objects.create(nome=nome, sobrenome=sobrenome, email=email, data_nascimento=data_nascimento)
+    return render(request, "index.html")
+
+def editar(request, id):
+    pessoa = Pessoa.objects.get(id=id)
     
-    pessoas = Pessoa.objects.all()
-    return render(request, "index.html", {"pessoas": pessoas})
+    if request.method == 'POST':
+        pessoa.nome = request.POST.get("nome")
+        pessoa.sobrenome = request.POST.get("sobrenome")
+        pessoa.email = request.POST.get("email")
+        pessoa.data_nascimento = request.POST.get("data_nascimento")
+        pessoa.save()
+        return redirect('home')
+    
+    return render(request, "update.html", {"pessoa": pessoa})
+
+def delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect('home')
+
+def pesquisar(request):
+    if request.method == 'GET':
+        termo_pesquisa = request.GET.get("search_query")
+        resultados = Pessoa.objects.filter(nome__icontains=termo_pesquisa)
+        return render(request, "index.html", {"pessoas": resultados})
+    
+    return redirect('home')
