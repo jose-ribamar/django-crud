49e04ca414ebd637e4b889c22ea1677ea9ba6228
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2023-06-23 23:35:12 -0300

    Create models.py
    Essas definições de modelo podem ser usadas para criar tabelas no banco de dados e manipular registros relacionados às pessoas

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
